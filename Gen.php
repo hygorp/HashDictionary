409ca32e4058702a56fb86a23d35a93af4b42852
Hygor Henrique <hygor.henrique@live.com>
2022-04-05 15:43:05 -0300

    Second Commit

--- Gen.php
+++ Gen.php
@@ -10,23 +10,23 @@
         <legend>Encrypt</legend>
         <div class="encrypt">
             <form method="post">
-                <label for="word-encrypt">Type Word: </label>
+                <label for="word-encrypt">Type a Word: </label>
                 <input type="text" id="word-encrypt" name="word-encrypt">
                 <button type="submit" name="send-encrypt">HaSh</button>
             </form>
             <?php
-                if($POST){
-                    if(isset($POST['send-encrypt'])){
-                        if(empty($POST['word-encrypt'])){
+                if($POST) {
+                    if (isset($POST['send-encrypt'])) {
+                        if (empty($POST['word-encrypt'])) {
                             echo "<p style='color: red'>Type a Word!!!!<p></p>";
-                        }else{
+                        } else {
                             $Core = new Core();
                             $word = $POST['word-encrypt'];
 
-                            for($i = 0; $i < strlen($word); $i++){
-                                for($j = 0; $j < count($Core->hashAlphabet); $j++){
-                                    if(md5($word[$i]) == $Core->hashAlphabet[$j]){
-                                        echo $Core->alphabet[$j]," - ", $Core->hashAlphabet[$j], "<br>";
+                            for ($i = 0; $i < strlen($word); $i++) {
+                                for ($j = 0; $j < count($Core->hashAlphabet); $j++) {
+                                    if (md5($word[$i]) == $Core->hashAlphabet[$j]) {
+                                        echo $Core->alphabet[$j], " - ", $Core->hashAlphabet[$j], "<br>";
                                     }
                                 }
                             }
@@ -39,9 +39,20 @@
     <fieldset>
         <legend>Decrypt</legend>
         <form method="post">
-            <label for="word-decrypt">Type a Word</label>
+            <label for="word-decrypt">Type a Hash: </label>
             <input type="text" id="word-decrypt" name="word-decrypt">
-            <button type="submit" name="send-decrypt">HaSh</button>
+            <button type="submit" name="send-decrypt">DeCr</button>
+            <?php
+                if($POST){
+                    if(isset($POST['send-decrypt'])){
+                        if(empty($POST['word-decrypt'])){
+                            echo "<p style='color: red'>Type a Word!!!!<p></p>";
+                        }else{
+                            //code here
+                        }
+                    }
+                }
+            ?>
         </form>
     </fieldset>
 </html>
